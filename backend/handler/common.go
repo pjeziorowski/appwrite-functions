b68ecb25d8caf89b8ca419d2810233f7b1680141
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-11-17 23:23:03 +0100

    feat: create project

--- backend/handler/common.go
+++ backend/handler/common.go
@@ -3,10 +3,12 @@ package handler
 import "github.com/dgrijalva/jwt-go"
 
 type JwtClaims struct {
-	HasuraClaims HasuraClaims `json:"https://hasura.io/jwt/claim"`
+	HasuraClaims HasuraClaims `json:"https://hasura.io/jwt/claims"`
 	jwt.StandardClaims
 }
 
 type HasuraClaims struct {
-	XHasuraUserId string `json:"X-Hasura-User-Id"`
+	XHasuraUserId       string `json:"x-hasura-user-id"`
+	XHasuraDefaultRole  string `json:"x-hasura-default-role"`
+	XHasuraAllowedRoles string `json:"x-hasura-allowed-roles"`
 }
